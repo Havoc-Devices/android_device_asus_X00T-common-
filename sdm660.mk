4b2e5839493ceb00f4ae29fb9af926b3df75b9f6
AT WORLD <atharvsutar3102003@gmail.com>
2022-07-24 11:25:35 +0530

    sdm660-common: Disable Blur by Default

--- sdm660.mk
+++ sdm660.mk
@@ -77,6 +77,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Blurs
+PRODUCT_PROPERTY_OVERRIDES  += \
+    persist.sys.sf.disable_blurs=1
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
